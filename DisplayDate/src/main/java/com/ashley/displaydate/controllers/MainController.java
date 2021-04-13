5767673f65bcc4890c342a2dc5fd94f93f520232
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-04-13 14:45:43 -0700

    display date project added

---
+++ DisplayDate/src/main/java/com/ashley/displaydate/controllers/MainController.java
@@ -0,0 +1,68 @@
+package com.ashley.displaydate.controllers;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+//import java.fucking.kitchenSink
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller //this is what returns jsp, also known as a decorator
+//****only one per controller******
+//@RestController
+//@RequestMapping
+public class MainController {
+
+
+
+	@RequestMapping("/")
+	public String home() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/date")
+	public String date(Model model) {
+		model.addAttribute("date", new Date());
+		SimpleDateFormat formatter = new SimpleDateFormat("EEEE, 'the' d 'of' MMMM, yyyy");
+		String date = formatter.format(new Date());
+		System.out.println(date);
+		return "date.jsp";
+	}
+	
+//	@RequestMapping("/time")
+//	public String getLocalTimeFromClock(char[] LocalTime) {
+//		System.out.println(LocalTime);
+//	        return "time.jsp";
+//	    }
+	
+	@RequestMapping("/time")
+	public String time(Model model) {
+		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+		String date = formatter.format(new Date());
+		System.out.println(date);
+		model.addAttribute("time", date);
+		return "time.jsp";
+
+}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DisplayDate/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet"
+	href="/webjars/bootstrap/4.5.0/css/bootstrap.min.css" />
+<script src="/webjars/jquery/3.5.1/jquery.min.js"></script>
+<script src="/webjars/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="/css/main.css" />
+<title>JAVA SUCKS</title>
+</head>
+<body>
+	<a href="/date"><h1>Date</h1></a>
+	<br />
+	<br />
+	<br />
+	<a href="/time"><h1>Time</h1></a>
+</body>
+</html>
