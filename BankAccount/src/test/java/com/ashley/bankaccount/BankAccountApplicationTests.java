9f6012c095ad7d0a1379aa455386bf3ba71422a5
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-04-13 19:46:36 -0700

    Bank Account assignment added

---
+++ BankAccount/src/test/java/com/ashley/bankaccount/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashley.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
